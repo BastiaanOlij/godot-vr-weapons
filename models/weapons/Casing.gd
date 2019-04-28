7af75335fa117985ead361cad03cdb7f30970881
Bastiaan Olij <mux213@gmail.com>
2019-04-28 20:54:24 +1000

    Part 4 - animation of our gun, audio and haptic feedback

---
+++ models/weapons/Casing.gd
@@ -0,0 +1,8 @@
+extends RigidBody
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_Lifetime_timeout():
+	queue_free()
--- models/weapons/m1911.gd
+++ models/weapons/m1911.gd
@@ -1,10 +1,33 @@
 extends "res://addons/vr-common/objects/Object_pickable.gd"
 
+export (PackedScene) var casing = null
+export var initial_casing_velocity = 5.0
+export var ammunition = 12
+
+export var rumble = 0.0 setget set_rumble, get_rumble
+
+func set_rumble(new_value):
+	if by_controller:
+		by_controller.rumble = new_value
+
+func get_rumble():
+	if by_controller:
+		return by_controller.rumble
+	else:
+		return 0.0
+
 func action():
-	if $Aim.is_colliding():
-		var what_did_we_hit = $Aim.get_collider()
-		if what_did_we_hit.has_method("hit"):
-			what_did_we_hit.hit($Aim.get_collision_point())
+	if !$AnimationPlayer.is_playing():
+		if ammunition > 0:
+			ammunition = ammunition - 1
+			$AnimationPlayer.play("Fire")
+			
+			if $Aim.is_colliding():
+				var what_did_we_hit = $Aim.get_collider()
+				if what_did_we_hit.has_method("hit"):
+					what_did_we_hit.hit($Aim.get_collision_point())
+		else:
+			$AnimationPlayer.play("EmptyClick")
 
 func _process(delta):
 	if $Aim.is_colliding():
@@ -13,3 +36,9 @@ func _process(delta):
 	else:
 		$Aim/Target_dot.visible = false
 
+func _emit_casing():
+	var new_casing = casing.instance()
+	new_casing.transform = $Pivot/Casing_spawn_point.global_transform
+	new_casing.linear_velocity = new_casing.transform.basis.x * initial_casing_velocity
+	
+	get_node("/root/Main/Viewport-VR/Spawns").add_child(new_casing)
