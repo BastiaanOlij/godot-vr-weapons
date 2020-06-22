e566b7d284b5c05bebb293603b2d0bb9bf7d0fa5
Bastiaan Olij <mux213@gmail.com>
2020-06-22 18:50:13 +1000

    Part 8, implement scope

--- models/weapons/Sniper/Sniper.gd
+++ models/weapons/Sniper/Sniper.gd
@@ -5,6 +5,29 @@ export var initial_bullet_speed = 300.0
 
 var can_shoot = true
 
+func pick_up(by, with_controller):
+	.pick_up(by, with_controller)
+	
+	with_controller.connect('button_pressed', self, 'button_pressed')
+	
+	$Scope.render_target_update_mode = Viewport.UPDATE_ALWAYS
+
+func let_go(starting_linear_velocity = Vector3(0.0, 0.0, 0.0)):
+	if picked_up_by:
+		picked_up_by.disconnect('button_pressed', self, 'button_pressed')
+	
+	.let_go(starting_linear_velocity)
+	
+	$Scope.render_target_update_mode = Viewport.UPDATE_DISABLED
+
+func button_pressed(button):
+	if (button == 1):
+		$Scope/Camera.fov = clamp($Scope/Camera.fov + 1, 1, 6)
+	if (button == 7):
+		$Scope/Camera.fov = clamp($Scope/Camera.fov - 1, 1, 6)
+	
+	$Scope/ScopeUI.set_scope($Scope/Camera.fov)
+
 func action():
 	if can_shoot and bullet:
 		# shoot our bullet
